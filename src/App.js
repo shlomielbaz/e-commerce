1c4fee8036e50f861325f6af905d131b1fb6aae4
Shlomi Elbaz <shlomisderot@gmail.com>
2020-11-05 15:48:00 +0200

    updaing our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { React } from 'react';
 import './App.css';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import HomePage  from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+
+// import HomePage from './pages/homepage/homepage.component';
 
 ReactDOM.render(
   <React.StrictMode>
