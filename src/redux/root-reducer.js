e14a5c6e693d56839f67864fb01cea8a83c8c828
Shlomi Elbaz <shlomisderot@gmail.com>
2020-11-24 12:52:35 +0200

    added redux-persist and set configuration for it, and use it for cart component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistsConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+
+export default persistReducer(persistsConfig, rootReducer);
